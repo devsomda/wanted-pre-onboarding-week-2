dc9b0188497fcdac55d6f2c93365ba235dac5d46
Dasom Park <dev.somda@gmail.com>
2023-08-29 21:37:42 +0900

    feat: 기본 type 분리

---
+++ src/types/Issues.tsx
@@ -0,0 +1,15 @@
+export interface Iuser {
+  avatar_url: string;
+  login: string;
+}
+
+export interface IissueSummary {
+  number: number;
+  title: string;
+  user: Iuser;
+  created_at: string;
+  comments: number;
+  id: number;
+}
+
+export type IissueList = IissueSummary[];
